62f9ab93515aef032cb988ebee9a196f18d41d83
youva <kadji_y@etna-alternance.net>
2023-01-26 15:15:56 +0100

    second  commit

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const url = "https://beacon.nist.gov/beacon/2.0/chain/last/pulse/last";
+
 
 function App() {
+  const [data, setData] = useState();
+
+  const getRandomValue = () => {
+    axios.get(url).then((res) => {
+      console.log(res.data.pulse.outputValue)
+      setData(res.data.pulse.outputValue)
+    })
+  }
+
+
+  const onClickeHandler = () => {
+    getRandomValue();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Test for Datanow
         </p>
+        <button className='button' onClick={onClickeHandler}>
+          <label htmlFor="text">Show me the last random value</label>
+        </button>
+        <p style={{width: '500px'}}> {data} </p>
+        
         
       </header>
     </div>
